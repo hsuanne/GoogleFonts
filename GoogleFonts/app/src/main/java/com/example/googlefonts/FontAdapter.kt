e9d41d0ec3b5584e13d0cc9a3ee3b0bb032ddb83
許安霓 <hsuanne6@gmail.com>
2021-08-09 18:40:07 +0800

    check for offline

--- GoogleFonts/app/src/main/java/com/example/googlefonts/FontAdapter.kt
+++ GoogleFonts/app/src/main/java/com/example/googlefonts/FontAdapter.kt
@@ -1,25 +1,35 @@
 package com.example.googlefonts
 
+import android.content.Context
 import android.graphics.Typeface
+import android.net.ConnectivityManager
+import android.net.NetworkCapabilities
+import android.os.Build
 import android.os.Handler
 import android.os.HandlerThread
 import android.text.method.ScrollingMovementMethod
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
 import android.widget.Toast
+import androidx.annotation.FontRes
+import androidx.annotation.RequiresApi
 import androidx.cardview.widget.CardView
+import androidx.core.content.res.ResourcesCompat
 import androidx.core.provider.FontRequest
 import androidx.core.provider.FontsContractCompat
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
+import java.util.*
 
 
-class FontAdapter(var viewModel: FontViewModel) :
+class FontAdapter(var viewModel: FontViewModel, var activity: MainActivity) :
     ListAdapter<String, RecyclerView.ViewHolder>(DiffCallback()) {
-    lateinit private var mHandler: Handler
+    private lateinit var mHandler: Handler
+    private lateinit var tf: Typeface
 
     inner class StrViewHolder(view: View) : RecyclerView.ViewHolder(view) {
         val textView: TextView = view.findViewById(R.id.str_container_textview)
@@ -32,39 +42,59 @@ class FontAdapter(var viewModel: FontViewModel) :
         return StrViewHolder(view)
     }
 
+    @RequiresApi(Build.VERSION_CODES.M)
     override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
         when (holder) {
             is StrViewHolder -> {
                 val str = getItem(position)
+
+
                 holder.apply {
                     textView.text = str
+
                     cardView.setOnClickListener {
-                        val handlerThread = HandlerThread("fonts")
-                        handlerThread.start()
-                        mHandler = Handler(handlerThread.looper)
+                        if (isOnline(activity)) {
+                            val handlerThread = HandlerThread("fonts")
+                            handlerThread.start()
+                            mHandler = Handler(handlerThread.looper)
 
-                        val request = FontRequest(
-                            "com.google.android.gms.fonts",
-                            "com.google.android.gms",
-                            str,
-                            R.array.com_google_android_gms_fonts_certs
-                        )
+                            val request = FontRequest(
+                                "com.google.android.gms.fonts",
+                                "com.google.android.gms",
+                                str,
+                                R.array.com_google_android_gms_fonts_certs
+                            )
 
-                        val callback = object : FontsContractCompat.FontRequestCallback() {
-                            override fun onTypefaceRetrieved(typeface: Typeface) {
-                                viewModel.currentTypeFace.value = typeface
+                            val callback = object : FontsContractCompat.FontRequestCallback() {
+                                override fun onTypefaceRetrieved(typeface: Typeface) {
+                                    viewModel.currentTypeFace.value = typeface
+                                }
+
+                                override fun onTypefaceRequestFailed(reason: Int) {
+                                    Toast.makeText(
+                                        itemView.context,
+                                        "Failed reason: $reason",
+                                        Toast.LENGTH_SHORT
+                                    ).show()
+                                }
                             }
-
-                            override fun onTypefaceRequestFailed(reason: Int) {
-                                Toast.makeText(
-                                    itemView.context,
-                                    "Failed reason: $reason",
-                                    Toast.LENGTH_LONG
-                                ).show()
-                            }
+                            FontsContractCompat
+                                .requestFont(itemView.context, request, callback, mHandler)
+                        } else {
+                            println(str)
+                            val lowerStr = str.replace(" ", "_").toLowerCase(Locale.ROOT)
+                            viewModel.currentTypeFace.value =
+                                try {
+                                    Typeface.createFromAsset(activity.assets, "$lowerStr.ttf")
+                                } catch (e: Exception) {
+                                    Toast.makeText(
+                                        itemView.context,
+                                        "error: $e",
+                                        Toast.LENGTH_SHORT
+                                    ).show()
+                                    Typeface.createFromAsset(activity.assets, "abeezee.ttf")
+                                }
                         }
-                        FontsContractCompat
-                            .requestFont(itemView.context, request, callback, mHandler)
                     }
                 }
             }
@@ -80,4 +110,27 @@ class DiffCallback : DiffUtil.ItemCallback<String>() {
     override fun areContentsTheSame(oldItem: String, newItem: String): Boolean {
         return oldItem == newItem
     }
+}
+
+@RequiresApi(Build.VERSION_CODES.M)
+fun isOnline(context: Context): Boolean {
+    val connectivityManager =
+        context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+    if (connectivityManager != null) {
+        val capabilities =
+            connectivityManager.getNetworkCapabilities(connectivityManager.activeNetwork)
+        if (capabilities != null) {
+            if (capabilities.hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR)) {
+                Log.i("Internet", "NetworkCapabilities.TRANSPORT_CELLULAR")
+                return true
+            } else if (capabilities.hasTransport(NetworkCapabilities.TRANSPORT_WIFI)) {
+                Log.i("Internet", "NetworkCapabilities.TRANSPORT_WIFI")
+                return true
+            } else if (capabilities.hasTransport(NetworkCapabilities.TRANSPORT_ETHERNET)) {
+                Log.i("Internet", "NetworkCapabilities.TRANSPORT_ETHERNET")
+                return true
+            }
+        }
+    }
+    return false
 }
--- GoogleFonts/app/src/main/java/com/example/googlefonts/MainActivity.kt
+++ GoogleFonts/app/src/main/java/com/example/googlefonts/MainActivity.kt
@@ -1,11 +1,16 @@
 package com.example.googlefonts
 
+import android.graphics.Typeface
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
 import android.widget.TextView
+import androidx.annotation.RequiresApi
+import androidx.core.content.res.ResourcesCompat
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.RecyclerView
+import java.util.*
 
 
 class MainActivity : AppCompatActivity() {
@@ -13,6 +18,7 @@ class MainActivity : AppCompatActivity() {
     private lateinit var recyclerView: RecyclerView
     private lateinit var adapter: FontAdapter
 
+    @RequiresApi(Build.VERSION_CODES.M)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -21,8 +27,15 @@ class MainActivity : AppCompatActivity() {
 
         textPreview = findViewById(R.id.text_preview)
         recyclerView = findViewById(R.id.recyclerview)
-        adapter = FontAdapter(fontViewModel)
+        adapter = FontAdapter(fontViewModel, this)
         recyclerView.adapter = adapter
+
+        if(!isOnline(this)){
+            val mFamilyNameSet = mutableListOf<String>()
+            mFamilyNameSet.addAll(listOf(*resources.getStringArray(R.array.family_names)))
+            adapter.submitList(mFamilyNameSet)
+        }
+
         fontViewModel.fontList.observe(this){
             adapter.submitList(it)
         }
